ac6abcdbf2ba3a840312171881e7c3e19de49f02
luis10barbo <luis10barbo2020@gmail.com>
2026-03-17 22:11:31 -0300

    fix: Added new domain to webconfig spring

--- server/src/main/java/br/com/luisbrb/portifolio/springboot/security/WebConfig.java
+++ server/src/main/java/br/com/luisbrb/portifolio/springboot/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3010", "https://angular.luisbrb.com.br/", "https://luisbrb.com.br/", "http://192.168.0.195:3010/")
+                .allowedOrigins("http://localhost:3010", "https://angular.luisbrb.com.br/", "https://luisbrb.com.br/", "http://192.168.0.195:3010/", "https://luisnmello.com.br/")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
